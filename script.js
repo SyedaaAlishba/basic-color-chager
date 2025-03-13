1d393cb1d9db35d069a3c30e73c85c8dfb76a2e4
SyedaaAlishba <syedaw.Alishba@gmail.com>
2025-03-13 12:25:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeColor(color) {
+  if (color == "red") {
+    document.body.style.backgroundColor = "red";
+  } else if (color == "yellow") {
+    document.body.style.backgroundColor = "yellow";
+  } else if (color == "green") {
+    document.body.style.backgroundColor = "green";
+  } else if (color == "blue") {
+    document.body.style.backgroundColor = "blue";
+  } else {
+    document.body.style.backgroundColor = "antiquewhite";
+  }
+}
